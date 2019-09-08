19896ab84718f902d9f7c14ac70e89c58360b455
asd <varroz56@gmail.com>
2019-09-08 17:28:46 +0100

    resolve syntax errors

--- src/Game.js
+++ src/Game.js
@@ -26,14 +26,19 @@ var Game = new Phaser.Class({
         for (var i = 0; i < playFieldSize; i++) {
             this.playFieldArray[i] = [];
             for (var j = 0; j < playFieldSize; j++) {
+                //allocate a sprite object using tileCoordinate function to determine the tile's location on the canvas
                 var emptyTile = this.add.sprite(this.tileCoordinate(j), this.tileCoordinate(i), "tiles");
+                //set the tile to invisible
                 emptyTile.alpha = 0;
                 emptyTile.visible = 0;
+                //add the tile to the group
                 this.playFieldGroup.add(emptyTile);
+                //add the tile/sprite object to the array current position and give initial value
                 this.playFieldArray[i][j] = {
-                    tileValue = 0,
+                    tileValue: 0,
                     tileSprite: emptyTile,
-                    upgradeable = true
+                    //declare that the tile is not on maximum level/value
+                    upgradeable : true
                 }
 
             }
