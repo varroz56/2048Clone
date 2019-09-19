f25af7b9b4f3c6c4818574371c3d74137c038694
asd <varroz56@gmail.com>
2019-09-19 13:21:21 +0100

    Devide code into sections for better readability

--- src/gameConfig.js
+++ src/gameConfig.js
@@ -1,4 +1,17 @@
-//configure game variable 
+/***************gameConfig.js file structure
+ * Sections:
+ * Declaration of Global variables
+ * 
+ * Window.onload function to calculate initial game scale and to initialize Phaser Game and to point to the Game scene
+ * 
+ * Content Scale with resize function including the add and rm Content functions to react to window size change and to add or remove content regarding to the given window size
+ *
+ *  Session storage section to store and read session storage variables to prevent data loss due refresh of the page
+ */
+
+//------------------------Global Variables--------------------------
+
+ //configure game variable 
 var game;
 //create score and scoretext global var to set to update in pgrade function
 var score=0;
@@ -14,6 +27,8 @@ var gameOptions = {
     btnWidth : 200,
     btnHeight :100
 }
+
+//-----------------------Window.onload----------------------------------
 
 //set the configuration of the canvas when the window loads
 window.onload = function () {
@@ -38,6 +53,9 @@ window.onload = function () {
     // add event listener to follow window resize and call resize function
     window.addEventListener("resize", resize, false);
 }
+
+//------------------------Content Scale-------------------------------
+
 //create resize function to calibrate the canvas size to any window size
 function resize() {
     //select canvas element of the window
@@ -55,19 +73,57 @@ function resize() {
     if (windowRatio < gameRatio) {
         canvas.style.width = windowWidth + "px";
         canvas.style.height = (windowWidth / gameRatio) + "px";
+        
+        //set header and footer height and width to fit to the canvas height
         header.style.width = windowWidth + "px";
         header.style.height = (windowHeight-(windowWidth / gameRatio))/2 + "px";
         footer.style.width = windowWidth + "px";
         footer.style.height =  (windowHeight-(windowWidth / gameRatio))/2 + "px";
+        // add content to them as there is only space on top and bottom
         addContentHF();
     }
     //if the browser's window size more than the game size, set the canvas to the game size using the game ratio
     else {
         canvas.style.width = (windowHeight * gameRatio) + "px";
         canvas.style.height = windowHeight + "px";
+        //remove header and footer content as there is only space on the side
         rmContentHF();
     }
 }
+//to add content to the top on mobile but to the side on desktop
+function addContentHF(){
+    //To decide how much content can go on the header and footer
+    //need to check their size
+    var header = document.getElementById("header");
+    var size = header.style.height;
+    if(size<35){
+        document.getElementById("htitle").innerHTML="2048 Game";
+        document.getElementById("f2p").innerHTML="Just click on the options and have fun!";
+
+    }    
+    document.getElementById("h1p").innerHTML="Pull the tiles from side to side to add same values to the next power of 2!";
+    document.getElementById("h2p").innerHTML="Reach 2048, and You Win!";
+    document.getElementById("f1p").innerHTML="In the Menu you can choose to play on different sizes of grids listen to music!";
+}
+function rmContentHF(){
+    document.getElementById("htitle").innerHTML="";
+    document.getElementById("h1p").innerHTML="";
+    document.getElementById("h2p").innerHTML="";
+    document.getElementById("f1p").innerHTML="";
+    document.getElementById("f2p").innerHTML="";
+    document.getElementById("hextra").innerHTML="";
+    document.getElementById("fextra").innerHTML="";
+
+}
+//to add extra content and style if the header and footer enought big
+function addExtraHF(){
+    document.getElementById("hextra").innerHTML="Beat yourself or others! As long as you do not colse the window, it is going to remember to your best score!";
+    document.getElementById("fextra").innerHTML="The original 2048 Game was created by Gabriele Cirulli in 2014 and it is subject to the MIT licence.";
+
+}
+
+//------------------------------Session Storage-------------------------------------
+
 //check if there is a saved value in the session storage for the size of the play field
 // if not return 4 as the default
 function playSize(){
@@ -90,19 +146,4 @@ function musicState(){
         return 0;
     }
 
-}
-//to add content to the top on mobile but to the side on desktop
-function addContentHF(){
-    document.getElementById("htitle").innerHTML="2048 Game";
-    document.getElementById("h1p").innerHTML="Pull the tiles from side to side to add same values to the next power of 2!";
-    document.getElementById("h2p").innerHTML="Reach 2048, and You Win!";
-    document.getElementById("f1p").innerHTML="In the Menu you can choose to play on different sizes of grids listen to music!";
-    document.getElementById("f2p").innerHTML="Just click on the options and have fun!";
-}
-function rmContentHF(){
-    document.getElementById("htitle").innerHTML="";
-    document.getElementById("h1p").innerHTML="";
-    document.getElementById("h2p").innerHTML="";
-    document.getElementById("f1p").innerHTML="";
-    document.getElementById("f2p").innerHTML="";
 }
